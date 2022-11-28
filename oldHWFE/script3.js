c7b9bb9fd6cb279668c22f287afc64c0cb030c96
Inna <110892526+InnaMala@users.noreply.github.com>
2022-11-28 22:19:04 +0100

    hometasks

---
+++ oldHWFE/script3.js
@@ -0,0 +1,14 @@
+// 1)Создайте кнопку в html при нажатию на которую в консоль выводится "привет".
+
+// 2)Создайте блок в html c картинкой и кнопку. При нажатии на кнопку блок с картинкой должен получать border-radius 50px и overflow hidden. При нажатии второй раз на кнопку стили должны убираться и возвращаться к исходным. Реализуйте через classList.toggle()
+
+let todolist = document.body.querySelector('body');
+
+let pressBtn = botton.element.querySelector('botton');
+
+showBtn.addEventListener('click', pressBtn);
+
+function showText(event) => {
+    console.log('Hello')
+    
+}
